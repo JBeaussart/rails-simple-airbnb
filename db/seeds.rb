d753e5e58518a4bea173538afdb8ba7fa86e37c3
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-01-20 00:40:30 +0100

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flat..."
+
+london = { name: 'Light Garden Flat London', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75, number_of_guests: 3}
+paris = { name: 'Paris flat family', address: '20 avenue de clichy', description: 'Elegant and crazy view on the effel tour', price_per_night: 98, number_of_guests: 8}
+berlin = { name: 'Crazy house berlin', address: '43 route de la saucisse', description: 'deutch kalitat house', price_per_night: 63, number_of_guests: 9}
+
+[london, paris, berlin].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts "Finished!"
